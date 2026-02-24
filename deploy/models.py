399e7631f63fdc7d9e930057b1a7edc4ca2dff23
Loblesh bhartal <lovelessbhartal12@gmail.com>
2026-02-24 22:25:26 +0545

    test of project

--- deploy/models.py
+++ deploy/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.core.exceptions import ValidationError
+
+class Note(models.Model):
+    description = models.CharField(max_length=255)
+
+    def clean(self):
+        if len(self.description) < 10:
+            raise ValidationError("Description must be at least 10 characters long.")
--- deploy/tests.py
+++ deploy/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.test import TestCase
+from .models import Note
+from django.core.exceptions import ValidationError
+
+class NoteModelTest(TestCase):
+
+    def test_notes_can_be_created(self):
+        
+        note = Note.objects.create(
+            description="This is a valid note description"
+        )
+        self.assertEqual(note.description, "This is a valid note description")
+        self.assertEqual(Note.objects.count(), 1)
+
+
+    def test_error_occurs_if_description_is_less_than_10_chars_long(self):
+        """
+        Test that validation error occurs if description < 10 characters
+        """
+        note = Note(description="Too short")
+
+        with self.assertRaises(ValidationError):
+            note.full_clean()   
